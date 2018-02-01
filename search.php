0bffc9b4bf113fbce05980b97595f2f727d1d98a
Masaga8 <masaga8@gmail.com>
2018-02-01 14:57:41 +0100

    Search_PHP

---
+++ search.php
@@ -0,0 +1,72 @@
+<?php
+    mysql_connect("localhost", "root", "") or die("Error connecting to database: ".mysql_error());
+    /*
+        localhost - it's location of the mysql server, usually localhost
+        root - your username
+        third is your password
+         
+        if connection fails it will stop loading the page and display an error
+    */
+     
+    mysql_select_db("miod") or die(mysql_error());
+    /* miod is the name of database we've created */     
+?>
+
+
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <title>Search</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <link rel="stylesheet" type="text/css" href="style.css"/>
+</head>
+<body>
+ 
+
+<?php
+    $query = $_GET['query']; 
+    // gets value sent over search form
+     
+    $query = trim($str); 
+     
+    if(empty($query) = False){ // boolean true if the variable was empty or with spaces
+         
+        $query = htmlspecialchars($query); 
+        // changes characters used in html to their equivalents, for example: < to &gt;
+         
+        $query = mysql_real_escape_string($query);
+        // makes sure nobody uses SQL injection
+         
+        $raw_results = mysql_query("SELECT * FROM Microindel
+            WHERE (`Name` LIKE '%".$query."%')") or die(mysql_error());
+             
+        // * means that it selects all fields
+        // Microindel is the name of our table
+         
+        if(mysql_num_rows($raw_results) > 0){ // if one or more rows are returned do following
+             
+            while($results = mysql_fetch_array($raw_results)){
+            // $results = mysql_fetch_array($raw_results) puts data from database into array, while it's valid it does the loop
+             
+                echo "<p><h3>".$results['Name']."</h3>".$results['Info']."</p>";
+                // posts results gotten from database(title and text) you can also show id ($results['id'])
+            }
+             
+        }
+        else{ // if there is no matching rows do following
+            echo "No results";
+        }
+         
+    }
+    else{ // if query length is less than minimum
+        echo "Empty search. Write something, please!";
+    }
+?> 
+
+</body>
+</html>
+
+
+
+
