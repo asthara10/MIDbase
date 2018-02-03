91cc8dc3e2c7a70db648c186699c22a83ff67c93
comradeameba <d.a.g@hotmail.com.ar>
2018-02-03 18:45:32 +0100

    some things in search.php, but not much

--- Search_PHP/search.php
+++ Search_PHP/search.php
@@ -1,15 +1,18 @@
 <?php
-    mysql_connect("localhost", "root", "") or die("Error connecting to database: ".mysql_error());
+    $id = mysqli_connect("localhost", "root", "Krosis1-Nakin2") or die("Error connecting to database: ".mysqli_error());
     /*
         localhost - it's location of the mysql server, usually localhost
         root - your username
-        third is your password
+        third is your password 
          
         if connection fails it will stop loading the page and display an error
     */
      
-    mysql_select_db("miod") or die(mysql_error());
-    /* miod is the name of database we've created */     
+	$textFields = Array('Disease.Diseasename', 'Disease.idMIM', 'Gene.GeneName', 'Gene.idENSEMBL', 'Location.CythogeneticLocation', 'Microindel.Name', 'Reference.DB');
+	//Fields to look into on text searches. Eventually this will go to globals_miod.php
+
+    mysqli_select_db($id, "miod") or die(mysqli_error());
+    //miod is the name of database we've created     
 ?>
 
 
@@ -25,29 +28,33 @@
  
 
 <?php
-    $query = $_GET['query']; 
+    $query = $_GET['query'];
     // gets value sent over search form
-     
-    $query = trim($str); 
-     
-    if(empty($query) = False){ // boolean true if the variable was empty or with spaces
-         
+
+    $query = trim($query); 
+    //Removes withespaces at the beggining and ending of chain
+
+    if(strlen($query) > 0){ // boolean true if the variable was empty or with spaces
+    	foreach (array_values($textFields) as $field) {
+		echo $ORconds[] = "MATCH (" . $field . ") AGAINST ('" . $query . "' "
+. "IN BOOLEAN MODE)";
+	}
         $query = htmlspecialchars($query); 
         // changes characters used in html to their equivalents, for example: < to &gt;
          
-        $query = mysql_real_escape_string($query);
+        $query = mysqli_real_escape_string($query);
         // makes sure nobody uses SQL injection
          
-        $raw_results = mysql_query("SELECT * FROM Microindel
-            WHERE (`Name` LIKE '%".$query."%')") or die(mysql_error());
+        $raw_results = mysqli_query("SELECT * FROM Microindel
+            WHERE (`Name` LIKE '%".$query."%')") or die(mysqli_error());
              
         // * means that it selects all fields
         // Microindel is the name of our table
          
-        if(mysql_num_rows($raw_results) > 0){ // if one or more rows are returned do following
+        if(mysqli_num_rows($raw_results) > 0){ // if one or more rows are returned do following
              
-            while($results = mysql_fetch_array($raw_results)){
-            // $results = mysql_fetch_array($raw_results) puts data from database into array, while it's valid it does the loop
+            while($results = mysqli_fetch_array($raw_results)){
+            // $results = mysqli_fetch_array($raw_results) puts data from database into array, while it's valid it does the loop
              
                 echo "<p><h3>".$results['Name']."</h3>".$results['Info']."</p>";
                 // posts results gotten from database(title and text) you can also show id ($results['id'])
@@ -62,11 +69,21 @@
     else{ // if query length is less than minimum
         echo "Empty search. Write something, please!";
     }
+
+if ($_REQUEST['query1']) {
+	$ORconds = array();
+	foreach (array_values($textFields) as $field) {
+		$ORconds[] = "MATCH (" . $field . ") AGAINST ('" . $_REQUEST['query1'] . "' "
+. "IN BOOLEAN MODE)";
+}
+$ANDconds[] = "(" . join(" OR ", $ORconds) . ")";
+}
+
+
 ?> 
 
 </body>
 </html>
-
 
 
 
