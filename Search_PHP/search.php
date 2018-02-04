86aea1dab5bdb7519cc00c46113d025664af1e2f
comradeameba <d.a.g@hotmail.com.ar>
2018-02-04 19:47:22 +0100

    Searcher operative (more or less) and a rudimentary output

--- Search_PHP/search.php
+++ Search_PHP/search.php
@@ -11,7 +11,7 @@
 	$textFields = Array('Disease.DiseaseName', 'Disease.idMIM', 'Gene.GeneName', 'Gene.idENSEMBL', 'Microindel.Name','Microindel.Info', 'Reference.PMID', 'Reference.DB');
 	//Fields to look into on text searches. Previously settled as fullindexes Eventually this will go to globals_miod.php
 
-	$select_search = "SELECT distinct Microindel.Name,Gene.GeneName,Gene.idENSEMBL,ClinicalSignificance.Value 
+	$select_search = "SELECT distinct Microindel.Name,Location.StartGRCh38,Location.EndGRCh38,ClinicalSignificance.Value,Reference.PMID 
 FROM Microindel,Location,Gene,Microindel_has_ClinicalSignificance,ClinicalSignificance,Disease,Microindel_has_Disease,Microindel_has_Reference,Reference  WHERE Microindel.idMicroindel=Microindel_has_Disease.Microindel_idMicroindel AND Microindel_has_Disease.Disease_idDisease=Disease.idDisease AND Microindel.idMicroindel=Location.Microindel_idMicroindel AND Location.idLocation=Gene.Location_idLocation AND  Microindel.idMicroindel=Microindel_has_ClinicalSignificance.Microindel_idMicroindel AND Microindel_has_ClinicalSignificance.ClinicalSignificance_idClinicalSignificance=ClinicalSignificance.idClinicalSignificance AND Microindel.idMicroindel=Microindel_has_Reference.Microindel_idMicroindel AND 
 Microindel_has_Reference.Reference_idReference=Reference.idReference AND ( 
 ";
@@ -21,18 +21,6 @@ Microindel_has_Reference.Reference_idReference=Reference.idReference AND (
     mysqli_select_db($id, "miod") or die(mysqli_error());
     //miod is the name of database we've created     
 ?>
-
-
-
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-    <title>Search</title>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <link rel="stylesheet" type="text/css" href="style.css"/>
-</head>
-<body>
- 
 
 <?php
     $query = $_GET['query'];
@@ -55,7 +43,7 @@ echo "No syntactic errors\n\n";
 	}
 	//create a match() against() query for every field in textFields global variable 
 
-	$sql=$select_search.join(" OR ",$ORconds)." );";
+		$sql=$select_search.join(" OR ",$ORconds)." );";
 	//appends the OR conditions created by the query to the select basic search
 
         $raw_results = mysqli_query($id, $sql) or die(mysqli_error());
@@ -63,15 +51,9 @@ echo "No syntactic errors\n\n";
 	//Iterate on everyone of the FULLTEXT fields, looking for any word match
          
         if(mysqli_num_rows($raw_results) > 0){ // if one or more rows are returned do following
-             
-            while($results = mysqli_fetch_array($raw_results)){
-            // $results = mysqli_fetch_array($raw_results) puts data from database into array, while it's valid it does the loop
-             
-                echo "<p><h3>".$results['Name']."</h3>".$results['Info']."</p>";
-                // posts results gotten from database(title and text) you can also show id ($results['id'])
-            }
-             
+            $results =  mysqli_fetch_array($raw_results);
         }
+
         else{ // if there is no matching rows do following
             echo "No results";
         }
@@ -81,9 +63,35 @@ echo "No syntactic errors\n\n";
         echo "Empty search. Write something, please!";
     }
 
-//SELECT distinct Microindel.idMicroindel,Microindel.Name,Location.EndGRCh38  FROM Microindel,Location WHERE Microindel.idMicroindel=Location.Microindel_idMicroindel AND MATCH (Microindel.Name) AGAINST ("rs1305061" IN BOOLEAN MODE);
 ?> 
 
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <title>Search</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <link rel="stylesheet" type="text/css" href="style.css"/>
+</head>
+<body>
+	<h1>MIOD-Search results</h1>
+	<p>Number of hits: <?php echo count($GLOBALS['results']),"\n"; ?></p>
+	<table style="color:black;font-size:100%;font-family:courier;border: 1px solid black;border-collapse: collapse; border-spacing: 5px;padding: 10px;margin: 20px;">
+		<tr>
+			<th rowspan="2"><strong>Microindel name<strong></th>
+			<th colspan="2" ><strong>Location<strong></th>
+			<th rowspan="2"><strong>Clinical significance<strong></th>
+			<th rowspan="2"><strong>Reference PubmedID<strong></th>
+		</tr>
+		<tr>
+			<td><strong>Start<strong></th>
+			<td><strong>End<strong></th>
+		</tr>			
+		<?php
+			foreach ($GLOBALS['raw_results'] as $row){
+				echo "<tr><td><a Name=\"$query\">",$row['Name'],"</a></td><td>",$row['StartGRCh38'],"</td><td>",$row['EndGRCh38'],"</td><td>",$row['PMID'],"</td></tr>";
+			} 
+		?>	
+	</table>
 </body>
 </html>
 
