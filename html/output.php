d7a7bd804824544ac01c41006b6d4cd1d517b836
comradeameba <d.a.g@hotmail.com.ar>
2018-02-17 17:03:08 +0100

    things done in search.php and database being repared

---
+++ html/output.php
@@ -0,0 +1,86 @@
+ <?php
+    include "globals_miod.php";
+    //include globals
+
+    print(file_get_contents("./patró.html"));
+    //import common header from patró
+
+    $name = $_REQUEST['query'];
+
+    $sql = $select_search . "Microindel.Name = \"" . $name . "\");";
+
+    print($sql);
+
+    $raw_results = mysqli_query($id, $sql) or die(mysqli_error());
+    $results = mysqli_fetch_array($raw_results);
+    //Transform results in an associative array, where keys are SQL column names 
+
+    foreach ($results as $key => $value) {
+        if ($value == "-") {
+            $results[$key] = "Not avalible";
+        }
+    }
+    //Transforming all "-" into "Not avalible", for I think they are more appropiate in this part of the web
+
+    if($results['idENSEMBL'] != "Not avalible" ){
+        $genename = "ENSEMBL id: " . $results['idENSEMBL'];
+    }
+    elseif($results['GeneName'] != "Not avalible" ){
+        $genename = "Name: " . $results['GeneName'];
+    }
+    else{
+        $genename = "Name: Not avalible";
+    }
+    //Select a genename with content
+
+    if($results['DiseaseName'] != "Not avalible"){
+        $disease = "Disease name: " . $results['DiseaseName'];
+    }
+    elseif($results['idMIM'] != "Not avalible"){
+        $disease = "Disease idMIM: " . $results['idMIM'];
+    }
+    else{
+        $disease = "Disease name: Not avalible";
+    }
+    
+?> 
+<html>
+<head>
+    <title>Search</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <link rel="stylesheet" type="text/css" href="MIOD_styles.css"/>
+    <!--bootstrap-->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+    <!-- jQuery -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <!--datatables-->
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.11/css/jquery.dataTables.css">
+    <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.11/js/jquery.dataTables.js"></script>
+</head>
+<body>
+    <div class="output-container">
+        <h1><?php echo $results["Name"]; ?></h1>
+        <p><strong>Genomic location:</strong></p>
+        <ul type="none">
+            <li>Chromosome: <?php echo $results["Chromosome"]; ?></li>
+            <li>Start: <?php echo $results["StartGRCh38"]; ?></li>
+            <li>End: <?php echo $results["EndGRCh38"]; ?></li>
+        </ul>
+        <p><strong>Associated gene:</strong></p>
+        <ul type="none">
+            <li><?php echo $genename; ?></li>
+        </ul>
+        <p><strong>Clinical significance:</strong></p>
+        <ul type="none">
+            <li>Clinical significance: <?php echo $results["Value"] ?></li>
+            <li><?php echo $disease; ?></li>
+        </ul>
+        <p><strong>References:</strong></p>
+        <ul type="none">
+            <li>Pubmed id: <?php echo $results["PMID"] ?></li>
+            <li>Source database: <?php echo $results["DB"]; ?></li>
+        </ul>
+    </div>
+</body>
+</html>
+
--- html/search.php
+++ html/search.php
@@ -1,54 +1,7 @@
 <?php
-    $id = mysqli_connect("localhost", "admiod", "Microindels4") or die("Error connecting to database: ".mysqli_error());
-    /*
-        localhost - it's location of the mysql server, usually localhost
-        root - your username
-        third is your password 
-         
-        if connection fails it will stop loading the page and display an error
-    */
-	
-    $textFields = Array('Disease.DiseaseName',
-    'Disease.idMIM',
-    'Gene.GeneName',
-    'Gene.idENSEMBL',
-    'Microindel.Name',
-    'Microindel.Info',
-    'Reference.PMID', 
-    'Reference.DB');
-	//Fields to look into on text searches. Previously settled as fullindexes Eventually this will go to globals_miod.php
+    include "globals_miod.php";
+    //include globals
 
-	$select_search = "SELECT distinct 
-    Microindel.Name,
-    Location.StartGRCh38,Location.EndGRCh38,
-    ClinicalSignificance.Value,
-    Reference.PMID 
-    FROM 
-    Microindel,
-    Location,
-    Gene,
-    Microindel_has_ClinicalSignificance,
-    ClinicalSignificance,Disease,
-    Microindel_has_Disease,Microindel_has_Reference,
-    Reference 
-    WHERE 
-    Microindel.idMicroindel=Microindel_has_Disease.Microindel_idMicroindel AND 
-    Microindel_has_Disease.Disease_idDisease=Disease.idDisease AND 
-    Microindel.idMicroindel=Location.Microindel_idMicroindel AND 
-    Location.idLocation=Gene.Location_idLocation AND  
-    Microindel.idMicroindel=Microindel_has_ClinicalSignificance.Microindel_idMicroindel AND 
-    Microindel_has_ClinicalSignificance.ClinicalSignificance_idClinicalSignificance=ClinicalSignificance.idClinicalSignificance AND 
-    Microindel.idMicroindel=Microindel_has_Reference.Microindel_idMicroindel AND 
-    Microindel_has_Reference.Reference_idReference=Reference.idReference AND ( 
-";
-	/*The first part of any text search. Outputs the selected fields FROM the selected tables WHERE the primary keys and foreign keys 
-	match between them. It's complicate to understnad, ask me if any doubt (By: David)*/
-
-    mysqli_select_db($id, "miod") or die(mysqli_error());
-    //miod is the name of database we've created     
-?>
-
-<?php
     $query = $_GET['query'];
     // gets value sent over search form
 
@@ -75,7 +28,7 @@
     	$ORconds[] = "MATCH (" . $field . ") AGAINST ('" . $query . "' IN BOOLEAN MODE)";
 	   //create a match() against() query for every field in textFields global variable 
     }
-	$sql=$select_search.join(" OR ",$ORconds)." );";
+	$sql = $select_search.join(" OR ",$ORconds)." );";
 	//appends the OR conditions created by the query to the select basic search
 
     $raw_results = mysqli_query($id, $sql) or die(mysqli_error());
@@ -85,13 +38,15 @@
     if(mysqli_num_rows($raw_results) > 0){ // if one or more rows are returned do following
         $results =  mysqli_fetch_array($raw_results);
     }
-    var_dump($raw_results);
-    foreach ($raw_results as $row) {
-        print($row["Name"]);    
-    }
 
-    #import common header
+    $counter = 0;
+    foreach ($raw_results as $a) {
+        $counter++;
+    }
+    //A weird way for obtaining the number of hits
+
     print(file_get_contents("./patró.html"));
+    //import common header from patró   
 ?> 
 
 <html>
@@ -108,37 +63,44 @@
     <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.11/js/jquery.dataTables.js"></script>
 </head>
 <body>
-    <div class="output-header">
-    	<h1>MIOD-Search results</h1>
-    	<p><strong>Number of hits: </strong><?php echo count($GLOBALS['results']),"\n"; ?></p>
+    <div class="output-container">
+        <div>
+        	<h1>MIOD-Search results</h1>
+        	<p><strong>Number of hits: </strong><?php print($GLOBALS['counter']); ?></p>
+        </div>
+        <div class="output">
+        	<table id="dataTable"> 
+        		<thead>
+                    <tr>
+            			<th><strong>Microindel name<strong></th>
+            			<th><strong>Clinical significance<strong></th>
+                        <th><strong>Associated Disease</strong></th>
+            			<th><strong>Reference PubmedID<strong></th>
+                    </tr>
+        		</thead>
+                <tbody>
+        				<?php
+        			foreach ($GLOBALS['raw_results'] as $row){
+        				echo "
+                        <tr><td><a href=\"output.php?query=",$row['Name'],"\">",$row['Name'],"</a></td>
+                        <td>",$row['Value'],"</td>";
+                        if($row['DiseaseName'] != "-"){
+                            echo "<td>",$row['DiseaseName'],"</td>";
+                        }
+                        else{
+                            echo "<td>",$row['idMIM'],"</td>";
+                        }
+                        echo "<td>",$row['PMID'],"</td></tr>";
+        			} 
+        		?>
+                </tbody>	
+        	</table>
+        </div>
+        <script type="text/javascript">
+            $(document).ready(function () {
+            $('#dataTable').DataTable();
+        });
+        </script>
     </div>
-    <div class="output">
-    	<table id="dataTable">
-    		<thead>
-                <tr>
-        			<th rowspan="2"><strong>Microindel name<strong></th>
-        			<th colspan="2" ><strong>Location<strong></th>
-        			<th rowspan="2"><strong>Clinical significance<strong></th>
-        			<th rowspan="2"><strong>Reference PubmedID<strong></th>
-                </tr>
-                <tr>
-                    <td><strong>Start<strong></th>
-                    <td><strong>End<strong></th>
-                </tr>           
-    		</thead>
-            <tbody>
-    				<?php
-    			foreach ($GLOBALS['raw_results'] as $row){
-    				echo "<tr><td><a Name=\"$query\" href=\"output.php\">",$row['Name'],"</a></td><td>",$row['StartGRCh38'],"</td><td>",$row['EndGRCh38'],"</td><td>",$row['Value'],"</td><td>",$row['PMID'],"</td></tr>";
-    			} 
-    		?>
-            </tbody>	
-    	</table>
-    </div>
-    <script type="text/javascript">
-        $(document).ready(function () {
-        $('#dataTable').DataTable();
-    });
-    </script>
 </body>
 </html>
