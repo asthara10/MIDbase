e6ac346b14d2bd297f81a6aefdc7160bfc1d6153
julia <mirp.julia@gmail.com>
2018-03-05 21:55:20 +0100

    send mail modified

--- web_app/send_mail.php
+++ web_app/send_mail.php
@@ -1,11 +1,32 @@
 <?php
+
 include "globals_miod.php";
 
-$from = $_REQUEST['email_address'];
-$message = $_REQUEST['comments'];
+// Initializing variables
+$from = NULL;
+$message = NULL;
 
-// Store messages on sql 
+if (!empty($_REQUEST['email_address'])) and (!empty($_REQUEST['comments'])) {
+	// Store messages on sql
+	$from = $_REQUEST['email_address'];
+	$message = $_REQUEST['comments'];
 
-
+	$sql = "INSERT INTO Comments (email, comment) VALUES ('$from', '$message');";
+		if (mysqli_query($id, $sql)) {
+			echo '<script type="text/javascript">
+				alert("Your message has ben submited succesfully :)");
+			</script>';				
+		}
+		else {
+			echo "Error: " . $sqldis . "<br>" . mysqli_error($id);
+		}
+}
+else {
+	// All fields are required
+	echo '<script type="text/javascript">
+		alert("E-mail and message are required.");
+	</script>';
+	echo '<script>window.location="./public_html/Contact.php"</script>';
+}
 
 ?>
